a5cdc179e4c18bcd99ea0d97e67d4aab7ecd4caa
Oleksij Onysymchuk <oleksij.onysymchuk@gmail.com>
2016-11-26 01:28:27 +0200

    Template

---
+++ src/main/java/training/Template/ContactsPage.java
@@ -0,0 +1,16 @@
+package training.Template;
+
+/**
+ * Site's (projects) start  page.
+ */
+public class ContactsPage extends AbstractPageTemplate {
+
+    @Override
+    public void showBody() {
+
+        //
+        //   prints only body of start page
+        //
+
+    }
+}
